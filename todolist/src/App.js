6c4e9a07684c0844765db318e88b4112784b3b08
robertacintra <roo.moony@gmail.com>
2019-08-08 19:38:29 -0300

    Fixes edit function

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,7 +4,6 @@ import './App.scss'
 function App() {
   const [todoValue, setTodoValue] = useState('');
   const [todos, setTodo] = useState([])
-  const [edit, setEdit] = useState(false)
 
   //Creates todos and clears input
   const handleInput = e => {
@@ -15,7 +14,8 @@ function App() {
     e.preventDefault();    
     const todo = {
       value: todoValue,
-      done: false
+      done: false,
+      edit: false 
     }
     
     if(!todoValue) return;
@@ -28,25 +28,30 @@ function App() {
     const { id } = e.target.parentElement;
     todos.splice(id, 1)
     setTodo([...todos]);
-    setEdit(false);
   }
 
   //Mark todos as done
   const handleDone = e => {
-    const { id } = e.target.parentElement;
+    const { id } = e.target.parentElement.parentElement;
     todos[id].done = !todos[id].done
     setTodo([...todos])
-    setEdit(false);
   }
 
   //edit the task
-  // const handleEdit = e => {
-  //   setEdit(true);
-  // }
+  const handleEdit = e => {
+    const { id } = e.target.parentElement.parentElement;
+    todos[id].edit = true
+    setTodo([...todos])
+  }
 
-  // handleChange(e) {
-    
-  // }
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { id } = e.target.parentElement.parentElement;
+    const novoValor = document.getElementById(`edit-${e.target.parentElement.parentElement.id}`).value.trim();
+    todos[id].edit = false
+    todos[id].value = novoValor === '' ? todos[id].value : novoValor;
+    setTodo([...todos])
+  }
 
   return (
     <div className="todo-list">
@@ -58,10 +63,17 @@ function App() {
       <div className="todos">
         {
           todos && todos.map((todo, i) => (
-            <div className="todo-block" key={todo.value} id={i}>
-              <button className={todo.done ? 'done' : 'not-done'} onClick={handleDone}>{todo.value}</button>
-              
-              {/* <button className="edit-todo" onClick={handleChange}>edit</button>    */}
+            <div className={todo.edit ? 'todo-block edit' : 'todo-block'} key={i} id={i}>
+
+            <div className="todo-block__to-edit">
+                <button className={todo.done ? 'done' : 'not-done'} id={`item-${i}`} onClick={handleDone}>{todo.value}</button>
+                <button className="edit-todo" onClick={handleEdit}>edit</button>   
+              </div>
+
+              <form className="todo-block__to-save">
+                <input type="text" name="todo-item" id={`edit-${i}`} />
+                <button className="edit-todo" onClick={handleChange}>save</button>   
+              </form>
 
               <button className="delete-todo" onClick={handleDelete}>x</button>              
             </div>
